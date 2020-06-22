308c08afe945e4739393cf40877ea9636adf2310
山村歩 <ayugooayu@gmail.com>
2020-06-22 21:52:51 +0900

    更新

--- WEB-INF/sources/jp/bcat/ListServlet.java
+++ WEB-INF/sources/jp/bcat/ListServlet.java
@@ -2,24 +2,24 @@ package jp.bcat;
 import java.io.*;
 import javax.servlet.http.*;
 
-public class ListServlet extends HttpServlet{
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException{
-        BookCatalog catalog = BookCatalog.getInstance();
-        Book result[] = catalog.getBooks();
-        
-        response.setContentType("text/html; charset=Shift_JIS");
-        PrintWriter out = response.getWriter();
-        out.println("<HTML>");
-        out.println("<HEAD><TITLE>}‘ˆê——</TITLE></HEAD>");
-        out.println("<BODY><P>" + result.length + "€–Ú</P>");
-        BookWriter w = new BookWriter(out, " ");
-        for(int i = 0; i < result.length; i++){
-            out.println("<PRE>[" + (i+1) + "]:");
-            out.println("}‘IDF" + result[i].getBookId());
-            w.write(result[i]);
-            out.println("</PRE>");
-        }
-        out.println("<A href='./'>ƒƒjƒ…[‚É–ß‚é</A>");
-        out.println("</BODY></HTML>");
-    }
+public class ListServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		BookCatalog catalog = BookCatalog.getInstance();
+		Book result[] = catalog.getBooks();
+
+		response.setContentType("text/html; charset=Shift_JIS");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>å›³æ›¸ä¸€è¦§</TITLE></HEAD>");
+		out.println("<BODY><P>" + result.length + "é …ç›®</P>");
+		BookWriter w = new BookWriter(out, " ");
+		for (int i = 0; i < result.length; i++) {
+			out.println("<PRE>[" + (i+1) + "]:");
+			out.println(" å›³æ›¸ID: " + result[i].getBookId());
+			w.write(result[i]);
+			out.println("</PRE>");
+		}
+		out.println("<A href='./'>ãƒ¡ãƒ‹ãƒ¥ãƒ¼ã«æˆ»ã‚‹</A>");
+		out.println("</BODY></HTML>");
+	}
 }
